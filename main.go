bfccedbb24f9aa928a93cd7bd9ed5ca36295aebc
haihun99 <3519486391@qq.com>
2024-07-17 22:59:27 +0800

    增加登陆界面返回结果

--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 
-	"github.com/hailun99/gotest/tset"
-	"github.com/hailun99/gotest/util"
+	"github.com/labstack/echo/v4"
 )
 
-func init() {
-	fmt.Println(3456)
+func s1(c echo.Context) error {
+	return c.String(http.StatusOK, "hello,world")
 }
 
-func init2() {
-	fmt.Println(6789)
+// 定义一个结构体接收请求参数
+type loginReq struct {
+	Username string `json:"username"` // 用户名
+	Password string `json:"password"` // 密码
+}
+
+// 定义一个结构体返回响应结果
+type loginRes struct {
+	Token string `json:"token"`
+}
+
+func login(c echo.Context) error {
+	req := &loginReq{}
+
+	// 绑定请求参数值
+	c.Bind(req)
+	fmt.Println(req.Username)
+	fmt.Println(req.Password)
+
+	res := &loginRes{}
+
+	// 判断用户名密码是否正确
+	if req.Username == "root" && req.Password == "123456" {
+
+		// 正确返回结果
+		res.Token = "abcd"
+		return c.JSON(200, res)
+
+	} else {
+		// 否则返回错误
+		return errors.New("用户名密码错误")
+	}
 }
 
 func main() {
-	fmt.Println("Hello World!")
-	r := util.Add(1, 2)
-	fmt.Println(r)
+	// 调用echo通过New赋值给e
+	e := echo.New()
 
-	tset.Abc()
+	e.Static("/", "static")
 
-	tset.Ccc()
-
-	tset.Iff()
-
+	e.POST("/api/login", login)
+	//e使用echo里的GET方法，调用上面 s1 与 s2
+	e.GET("/s1", s1)
+	// e调用方法，开始启动监听下面的端口号
+	e.Logger.Fatal(e.Start("0.0.0.0:1323"))
 }
